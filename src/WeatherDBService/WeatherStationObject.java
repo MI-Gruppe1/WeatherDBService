f98d79160fd34e8d3ec16624f9d89a468593c651
Hannes Berger <xaaris@googlemail.com>
2016-11-22 16:11:50 +0100

    cleanup

--- src/WeatherDBService/WeatherStationObject.java
+++ src/WeatherDBService/WeatherStationObject.java
@@ -20,6 +20,16 @@ public class WeatherStationObject {
 	public double getDistanceTo(double longitude, double latitude) {
 		return haversine(this.latitude, this.longitude, latitude, longitude);
 	}
+
+	private double haversine(double latStation, double lngStation, double latWaypoint, double lngWaypoint){
+        double dLat = Math.toRadians(latWaypoint - latStation);
+        double dLon = Math.toRadians(lngWaypoint - lngStation);
+        latStation = Math.toRadians(latStation);
+        latWaypoint = Math.toRadians(latWaypoint);
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(latStation) * Math.cos(latWaypoint);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return R * c;
+    }
 	
 	public int getId() {
 		return id;
@@ -36,15 +46,5 @@ public class WeatherStationObject {
 	public double getLatitude() {
 		return latitude;
 	}
-
-	private double haversine(double latStation, double lngStation, double latWaypoint, double lngWaypoint){
-        double dLat = Math.toRadians(latWaypoint - latStation);
-        double dLon = Math.toRadians(lngWaypoint - lngStation);
-        latStation = Math.toRadians(latStation);
-        latWaypoint = Math.toRadians(latWaypoint);
-        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(latStation) * Math.cos(latWaypoint);
-        double c = 2 * Math.asin(Math.sqrt(a));
-        return R * c;
-    }
 
 }
